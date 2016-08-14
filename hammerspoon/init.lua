1f94fc1fe824014d139869516f1f9c93dffb3569
woss <danijelmaricic@gmail.com>
2016-08-14 20:23:30 +0300

    cleanup of the hammerspoon

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,34 +1,34 @@
 local modalKey = {"alt"}
 
-local resizeMappings = {
-  h={x=0, y=0, w=0.5, h=1},
-  j={x=0, y=0.5, w=1, h=0.5},
-  k={x=0, y=0, w=1, h=0.5},
-  l={x=0.5, y=0, w=0.5, h=1},
-  m={x=0, y=0, w=1, h=1},
-  u={x=0, y=0, w=0.33, h=1},
-  i={x=0.33, y=0, w=0.33, h=1},
-  o={x=0.66, y=0, w=0.33, h=1},
+-- local resizeMappings = {
+--   h={x=0, y=0, w=0.5, h=1},
+--   j={x=0, y=0.5, w=1, h=0.5},
+--   k={x=0, y=0, w=1, h=0.5},
+--   l={x=0.5, y=0, w=0.5, h=1},
+--   m={x=0, y=0, w=1, h=1},
+--   u={x=0, y=0, w=0.33, h=1},
+--   i={x=0.33, y=0, w=0.33, h=1},
+--   o={x=0.66, y=0, w=0.33, h=1},
 
-}
+-- }
 
-for key in pairs(resizeMappings) do
-  hs.hotkey.bind(modalKey, key, function()
-    local win = hs.window.focusedWindow()
-    if win then win:moveToUnit(resizeMappings[key]) end
-  end)
-end
+-- for key in pairs(resizeMappings) do
+--   hs.hotkey.bind(modalKey, key, function()
+--     local win = hs.window.focusedWindow()
+--     if win then win:moveToUnit(resizeMappings[key]) end
+--   end)
+-- end
 
-hs.hotkey.bind(modalKey, "r", function()
-  hs.reload()
-end)
+-- hs.hotkey.bind(modalKey, "r", function()
+--   hs.reload()
+-- end)
 
 local focusKeys = {
   s='Safari',
   a='Atom',
   c='Safari',
   w='Mail',
-  -- c='Google Chrome',
+  c='Google Chrome',
   -- b='Google Chrome Canary',
   d='Slack',
   f='iTerm',
